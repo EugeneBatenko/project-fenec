52adc3a69ae8787fb5f6f0d286c970a794b587e2
Yevhen Batenko <batenko.e.v@gmail.com>
2018-09-11 16:33:50 +0300

    Add slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,19 @@
+//MOBILE MENU
 var mob_menu = document.getElementById("mobile-nav");
 
 function hamburger(x) {
     x.classList.toggle("open");
     mob_menu.classList.toggle("mobile-ul-active");
 };
+//SLIDER
+$(document).ready(function () {
+    $('.header-slider').slick({
+        dots: false,
+        infinite: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 800,
+    });
+});
