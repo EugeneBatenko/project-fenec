b0b5cd51a60d4c76da09bc0855f89d6dd94ded11
Yevhen Batenko <batenko.e.v@gmail.com>
2018-11-25 23:04:07 +0200

    Some decoration

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,16 @@ function hamburger(x) {
     x.classList.toggle("open");
     mob_menu.classList.toggle("mobile-ul-active");
 };
+//Sticky menu
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (currentScrollPos != 0) {
+        document.getElementById("header").style.background = "rgba(38,38,38,0.8)";
+    } else {
+        document.getElementById("header").style.background = "rgba(0,0,0,.0)";
+    }
+}
+
 //SLIDER
 $(document).ready(function () {
     $('.header-slider').slick({
