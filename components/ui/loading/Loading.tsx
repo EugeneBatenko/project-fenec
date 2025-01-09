fb2909d5bd285d1edf0102543a9d7a91ec0c19de
EugeneBatenko <batenko.e.v@gmail.com>
2025-01-09 16:44:45 +0200

    Change loader to more pretty

--- components/ui/loading/Loading.tsx
+++ components/ui/loading/Loading.tsx
@@ -1,12 +1,14 @@
 import {FC} from "react";
 import styles from "./loading.module.css";
+import LogoLoader from "@/public/logo-loader.svg";
 
 
 export const Loading: FC = () => {
     return (
         <div className={styles.loaderContainer}>
-            <div className={styles.loader}></div>
-            <p>Loading...</p>
+            <LogoLoader className={styles.loader}/>
+            <div className={styles.loadingLine}></div>
+            {/*<h2 className={styles.title}>Loading...</h2>*/}
         </div>
     )
 }
