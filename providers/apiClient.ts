fbe84bb0c3757e1a3f47850e85f604dc39cde144
EugeneBatenko <batenko.e.v@gmail.com>
2025-03-07 14:28:45 +0200

    Changed enviroment condition

--- providers/apiClient.ts
+++ providers/apiClient.ts
@@ -1,7 +1,7 @@
 const baseUrl =
-    process.env.NODE_ENV === "development"
-        ? "http://localhost:3000"
-        : process.env.NEXT_PUBLIC_API_BASE_URL || "https://my-production-url.com";
+    process.env.NODE_ENV !== "development"
+        ? process.env.NEXT_PUBLIC_API_BASE_URL || ""
+        : "http://localhost:3000";
 
 
 class ApiClient {
